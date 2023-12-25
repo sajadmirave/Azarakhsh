8507439c24160a814b4b70fa2b92a40804ffdb67
sajjad mirave <banditofsocorstars@gmail.com>
2023-12-25 12:13:56 -0800

    create model structure

---
+++ src/model.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+# get current path
+current_path = os.path.abspath(__file__)
+
+# get root directory path
+root = os.path.dirname(os.path.dirname(current_path))
+
+sys.path.append(root)
+
+from index import DB
+db = DB()
+
+class Model:
+    def __init__(self,table,cols):
+        self.table = table
+        self.cols = cols
+        self.fields = {}
+        self.__create()
+
+    def __create(self):
+        # create property for fields
+        self.__create_fields()
+        self.__create_table()
+
+    def __create_fields(self):
+        for key,name in self.cols.items():
+            setattr(self,key,name)
+            self.fields[key] = name
+
+    def __create_table(self):
+        db.model(self.table,self.cols)
--- test.py
+++ test.py
@@ -3,46 +3,62 @@ from src.type import Varchar, NotNull,AutoIncrement,Integer
 from src.columns import Column
 
 
-db = DB()
+# db = DB()
 
-# print(db.getAll('user'))
-# col = {
-#     "id":Column('id',Integer(),AutoIncrement()),
-#     "title":Column("title",Varchar(20),NotNull())
-# }
-# db.model("fruit",col)
+# # print(db.getAll('user'))
+# # col = {
+# #     "id":Column('id',Integer(),AutoIncrement()),
+# #     "title":Column("title",Varchar(20),NotNull())
+# # }
+# # db.model("fruit",col)
 
-# fruits_data = ["apple","cucumber",'peach',"avacado","melon",'lemon','tomato','potato']
+# # fruits_data = ["apple","cucumber",'peach',"avacado","melon",'lemon','tomato','potato']
 
-# for i in fruits_data:
-#     db.insert('fruit',{
-#         "title": i
-#     })
+# # for i in fruits_data:
+# #     db.insert('fruit',{
+# #         "title": i
+# #     })
 
 
-print(db.getAll('fruit',json=True))
-# result = db.getOne('fruit','title','apple')
-# result = db.getOne(table='fruit',field='title',value='apple')
-# print(result)
+# print(db.getAll('fruit',json=True))
+# # result = db.getOne('fruit','title','apple')
+# # result = db.getOne(table='fruit',field='title',value='apple')
+# # print(result)
 
-# print(db.exists(table='fruit',field='title',value='apple')
-# )
+# # print(db.exists(table='fruit',field='title',value='apple')
+# # )
 
-# print(db.count(table='fruit'))
+# # print(db.count(table='fruit'))
 
-# updated_data = {
-#     "title":"new_banana"
-# }
+# # updated_data = {
+# #     "title":"new_banana"
+# # }
 
-# condition = {
-#     "title":"banana",
-#     # "title1":"banana",
-# }
-# db.update('fruit',condition,updated_data)
-# db.update(table='fruit',data=updated_data,condition_field='title',condition_value='apple')
+# # condition = {
+# #     "title":"banana",
+# #     # "title1":"banana",
+# # }
+# # db.update('fruit',condition,updated_data)
+# # db.update(table='fruit',data=updated_data,condition_field='title',condition_value='apple')
 
-# db.delete('fruit',{
-#     'title':"tomato"
-# })
+# # db.delete('fruit',{
+# #     'title':"tomato"
+# # })
 
-db.close_connection()
+# db.close_connection()
+
+from src.model import Model
+
+
+fruit = Model('fruit',{
+    "title":"varchar",
+    "name":"di"
+})
+
+# fruit.create()
+print(fruit.name)
+
+# fruit.change()
+# print(fruit.test1)
+
+# fruit.create_property('hello')
